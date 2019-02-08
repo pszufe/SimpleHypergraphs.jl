75368d7755395148c3da9cb08e5289a0c25ed1f4
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-08 19:58:28 +0000

    build based on 3833ea4

--- dev/search_index.js
+++ dev/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Reference",
     "title": "SimpleHypergraphs.Hypergraph",
     "category": "type",
-    "text": "Hypergraph{T} <: AbstractMatrix{Union{T, Nothing}}\n\nA hypergraph storing information about vertices and hyperedges.\n\nConstructors\n\nHypergraph{T}(n,k) where {T<:Real}\n\nConstruct a hypergraph with a given number of vertices and hyperedges.\n\nHypergraph(m::AbstractMatrix{T}) where {T<:Real}\n\nConstruct a hypergraph using its matrix representation. In the matrix representation rows are vertices and columns are hyperedges.\n\nArguments\n\nT : type of values stored in the hypergraph\nn : number of vertices\nk : number of hyperedges\nm : a matrix representation rows are vertices and columns are hyperedges\n\n\n\n\n\n"
+    "text": "Hypergraph{T} <: AbstractMatrix{Union{T, Nothing}}\n\nA hypergraph storing information about vertices and hyperedges.\n\nConstructors\n\nHypergraph{T}(n,k) where {T<:Real}\nHypergraph{T,V}(n, k) where {T<:Real, V}\nHypergraph{T,V,E}(n, k) where {T<:Real, V, E}\n\nConstruct a hypergraph with a given number of vertices and hyperedges. Optionally, values of type V can be stored at vertices and values of type E can be stored at hyperedges.\n\nHypergraph(m::AbstractMatrix{Union{T, Nothing}}) where {T<:Real}\nHypergraph{V}(m::AbstractMatrix{Union{T, Nothing}}) where {T<:Real, V}\nHypergraph{V, E}(m::AbstractMatrix{Union{T, Nothing}}) where {T<:Real, V, E}\n\nConstruct a hypergraph using its matrix representation. In the matrix representation rows are vertices and columns are hyperedges. Optionally, values of type V can be stored at vertices and values of type E can be stored at hyperedges.\n\nArguments\n\nT : type of weight values stored in the hypergraph\nV : type of values stored in the vertices of the hypergraph\nE : type of values stored in the edges of the hypergraph\nn : number of vertices\nk : number of hyperedges\nm : a matrix representation rows are vertices and columns are hyperedges\n\n\n\n\n\n"
 },
 
 {
@@ -49,19 +49,51 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "reference/#SimpleHypergraphs.add_hyperedge!-Union{Tuple{Hypergraph{T}}, Tuple{T}} where T<:Real",
+    "location": "reference/#SimpleHypergraphs.add_hyperedge!-Union{Tuple{Hypergraph{T,V,E}}, Tuple{E}, Tuple{V}, Tuple{T}} where E where V where T<:Real",
     "page": "Reference",
     "title": "SimpleHypergraphs.add_hyperedge!",
     "category": "method",
-    "text": "add_hyperedge!(h::Hypergraph{T};vertices::Dict{Int,T} = Dict{Int,T}()) where T <: Real\n\nAdds a hyperedge to a given hypergraph h. Optionally, existing vertices can be added to the created hyperedge. The paramater vertices represents a dictionary of vertex identifiers and values stored at the hyperedges\n\n\n\n\n\n"
+    "text": "add_hyperedge!(h::Hypergraph{T, V, E}; vertices::Dict{Int,T} = Dict{Int,T}(),\n               hyperedge_meta::Union{E,Nothing}=nothing) where {T <: Real, V, E}\n\nAdds a hyperedge to a given hypergraph h. Optionally, existing vertices can be added to the created hyperedge. The paramater vertices represents a dictionary of vertex identifiers and values stored at the hyperedges. Additionally, a value can be stored with the hyperedge using the hyperedge_meta keyword parameter.\n\n\n\n\n\n"
 },
 
 {
-    "location": "reference/#SimpleHypergraphs.add_vertex!-Union{Tuple{Hypergraph{T}}, Tuple{T}} where T<:Real",
+    "location": "reference/#SimpleHypergraphs.add_vertex!-Union{Tuple{Hypergraph{T,V,E}}, Tuple{E}, Tuple{V}, Tuple{T}} where E where V where T<:Real",
     "page": "Reference",
     "title": "SimpleHypergraphs.add_vertex!",
     "category": "method",
-    "text": "add_vertex!(h::Hypergraph{T};hyperedges::Dict{Int,T} = Dict{Int,T}()) where T <: Real\n\nAdds a vertex to a given hypergraph h. Optionally, the vertex can be added to existing hyperedges. The hyperedges parameter presents a dictionary of hyperedge identifiers and values stored at the hyperedges\n\n\n\n\n\n"
+    "text": "add_vertex!(h::Hypergraph{T, V, E}; hyperedges::Dict{Int,T} = Dict{Int,T}(),\n            vertex_meta::Union{V,Nothing} nothing undef) where {T <: Real, V, E}\n\nAdds a vertex to a given hypergraph h. Optionally, the vertex can be added to existing hyperedges. The hyperedges parameter presents a dictionary of hyperedge identifiers and values stored at the hyperedges. Additionally, a value can be stored with the vertex using the vertex_meta keyword parameter.\n\n\n\n\n\n"
+},
+
+{
+    "location": "reference/#SimpleHypergraphs.set_vertex_meta!-Union{Tuple{E}, Tuple{V}, Tuple{T}, Tuple{Hypergraph{T,V,E},Union{Nothing, V},Int64}} where E where V where T<:Real",
+    "page": "Reference",
+    "title": "SimpleHypergraphs.set_vertex_meta!",
+    "category": "method",
+    "text": "set_vertex_meta!(h::Hypergraph{T, V, E}, new_value::Union{V,Nothing}, id::Int)\n    where {T <: Real, V, E}\n\nSets a new meta value new_value for the vertex id in the hypegraph h.\n\n\n\n\n\n"
+},
+
+{
+    "location": "reference/#SimpleHypergraphs.get_vertex_meta-Union{Tuple{E}, Tuple{V}, Tuple{T}, Tuple{Hypergraph{T,V,E},Int64}} where E where V where T<:Real",
+    "page": "Reference",
+    "title": "SimpleHypergraphs.get_vertex_meta",
+    "category": "method",
+    "text": "get_vertex_meta(h::Hypergraph{T, V, E}, id::Int) where {T <: Real, V, E}\n\nReturns a meta value stored at the vertex id in the hypergraph h.\n\n\n\n\n\n"
+},
+
+{
+    "location": "reference/#SimpleHypergraphs.set_hyperedge_meta!-Union{Tuple{E}, Tuple{V}, Tuple{T}, Tuple{Hypergraph{T,V,E},Union{Nothing, E},Int64}} where E where V where T<:Real",
+    "page": "Reference",
+    "title": "SimpleHypergraphs.set_hyperedge_meta!",
+    "category": "method",
+    "text": "set_hyperedge_meta!(h::Hypergraph{T, V, E}, new_value::Union{E,Nothing}, id::Int)\n    where {T <: Real, V, E}\n\nSets a new meta value new_value for the hyperedge id in the hypegraph h.\n\n\n\n\n\n"
+},
+
+{
+    "location": "reference/#SimpleHypergraphs.get_hyperedge_meta-Union{Tuple{E}, Tuple{V}, Tuple{T}, Tuple{Hypergraph{T,V,E},Int64}} where E where V where T<:Real",
+    "page": "Reference",
+    "title": "SimpleHypergraphs.get_hyperedge_meta",
+    "category": "method",
+    "text": "get_hyperedge_meta(h::Hypergraph{T, V, E}, id::Int) where {T <: Real, V, E}\n\nReturns a meta value stored at the hyperedge id in the hypergraph h.\n\n\n\n\n\n"
 },
 
 {
@@ -69,7 +101,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Reference",
     "title": "Manipulating vertices and hyperedges",
     "category": "section",
-    "text": "add_hyperedge!(::Hypergraph{T};::Dict{Int,T}) where T <: Real\nadd_vertex!(::Hypergraph{T};::Dict{Int,T}) where T <: Real"
+    "text": "\nadd_hyperedge!(::Hypergraph{T, V, E}; ::Dict{Int,T}, ::Union{E,Nothing} ) where {T <: Real, V, E}\nadd_vertex!(::Hypergraph{T, V, E};::Dict{Int,T},::Union{V,Nothing} ) where {T <: Real, V, E}\nset_vertex_meta!(::Hypergraph{T, V, E}, ::Union{V,Nothing}, ::Int) where {T <: Real, V, E}\nget_vertex_meta(::Hypergraph{T, V, E}, ::Int) where {T <: Real, V, E}\nset_hyperedge_meta!(::Hypergraph{T, V, E}, ::Union{E,Nothing}, ::Int) where {T <: Real, V, E}\nget_hyperedge_meta(::Hypergraph{T, V, E}, ::Int) where {T <: Real, V, E}"
 },
 
 {
@@ -85,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Reference",
     "title": "Base.setindex!",
     "category": "method",
-    "text": "Base.setindex!(h::Hypergraph, ::Nothing, idx::Vararg{Int,2})\n\nRemoves a vertex from a given hyperedge for a hypergraph h and a given vertex-hyperedge pair idx.\n\n\n\n\n\n"
+    "text": "Base.setindex!(h::Hypergraph, ::Nothing, idx::Vararg{Int,2})\n\nRemoves a vertex from a given hyperedge for a hypergraph h and a given vertex-hyperedge pair idx. Note that trying to remove a vertex from a hyperedge when it is not present will not throw an error.\n\n\n\n\n\n"
 },
 
 {
@@ -101,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Reference",
     "title": "Hypergraph array getters and setters",
     "category": "section",
-    "text": "Normally you work with a hypergraph via array setters, for example the code below craete an Hypergraph and add vertex one to hyperedges 2 and 3 with weight 5:h = Hypergraph{Int64}(2,3);\nh[1, 2:3] .= 5;  \nh\n\n# output\n\n2×3 Hypergraph{Int64}:\n nothing  5         5\n nothing   nothing   nothinggetindex(::Hypergraph, ::Vararg{Int,2})\nsetindex!(::Hypergraph, ::Nothing, ::Vararg{Int,2})\nsetindex!(::Hypergraph, ::Real, ::Vararg{Int,2})"
+    "text": "Normally you work with a hypergraph via array setters, for example the code below craete an Hypergraph and add vertex one to hyperedges 2 and 3 with weight 5:h = Hypergraph{Int64}(2,3);\nh[1, 2:3] .= 5;  \nh\n\n# output\n\n2×3 Hypergraph{Int64,Nothing,Nothing}:\n nothing  5         5\n nothing   nothing   nothinggetindex(::Hypergraph, ::Vararg{Int,2})\nsetindex!(::Hypergraph, ::Nothing, ::Vararg{Int,2})\nsetindex!(::Hypergraph, ::Real, ::Vararg{Int,2})"
 },
 
 {
